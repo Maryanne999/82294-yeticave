687ab79443218da69aa72c3e334c546fd2864c84
Maryanne999 <sofi.mishel2014@yandex.ru>
2017-08-27 16:36:16 +0300

    Выполнила задание №4

--- index.php
+++ index.php
@@ -44,6 +44,23 @@ $now = strtotime('now');
         <nav class="user-menu">
 
         <!-- здесь должен быть PHP код для показа аватара пользователя -->
+                 <?php if ($is_auth == "true") : ?>
+         <div class="user-menu__image">
+           <img src="$user_avatar" width="40" height="40" alt="Пользователь">
+         </div>
+         <div class="user-menu__logged">
+           <p>$user_name</p>
+         </div>
+         <?php else: ($is_auth == "false") : ?>
+    <ul class="user-menu__list">
+    <li class="user-menu__item">
+      <a href="#">Регистрация</a>
+    </li>
+    <li class="user-menu__item">
+      <a href="#">Вход</a>
+    </li>
+  </ul>
+        <?php endif; ?>
 
         </nav>
     </div>
