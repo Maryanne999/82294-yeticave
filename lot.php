a82aab873327910b2ea72489fd255bcb77676b37
Maryanne999 <sofi.mishel2014@yandex.ru>
2017-09-11 21:57:35 +0300

    Работа со строкой запроса - готовое задание

--- lot.php
+++ lot.php
@@ -26,8 +26,9 @@ function time_format($timestamp) {
  }
 };
 
-if (isset($_GET['lot_id']))  {
+if (isset($_GET['lot_id']) && isset($ads[$_GET['lot_id']]))  {
 	$lot_item = $_GET['lot_id'];
+	$lot = $ads[$lot_item];
 }
 else {
 	http_response_code (404);
@@ -93,14 +94,14 @@ else {
         </ul>
     </nav>
     <section class="lot-item container">
-        <h2><?=htmlspecialchars($lot_item['title']); ?></h2>
+        <h2><?=htmlspecialchars($lot['title']); ?></h2>
         <div class="lot-item__content">
             <div class="lot-item__left">
                 <div class="lot-item__image">
-                    <img src="<?=$lot_item['url']; ?>" width="730" height="548" alt="Сноуборд">
+                    <img src="<?=$lot['url']; ?>" width="730" height="548" alt="Сноуборд">
                 </div>
-                <p class="lot-item__category">Категория: <span><?=htmlspecialchars($lot_item); ?></span></p>
-                <p class="lot-item__description"><?=htmlspecialchars($lot_item['description']) ; ?></p>
+                <p class="lot-item__category">Категория: <span><?=htmlspecialchars($lot['categories']); ?></span></p>
+                <p class="lot-item__description"><?=htmlspecialchars($lot['description']) ; ?></p>
             </div>
             <div class="lot-item__right">
                 <div class="lot-item__state">
@@ -110,10 +111,10 @@ else {
                     <div class="lot-item__cost-state">
                         <div class="lot-item__rate">
                             <span class="lot-item__amount">Текущая цена</span>
-                            <span class="lot-item__cost"><?=$lot_item['price']; ?></span>
+                            <span class="lot-item__cost"><?=$lot['price']; ?></span>
                         </div>
                         <div class="lot-item__min-cost">
-                            Мин. ставка <span><?=$lot_item['price']; ?></span>
+                            Мин. ставка <span><?=$lot['price']; ?></span>
                         </div>
                     </div>
                     <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
